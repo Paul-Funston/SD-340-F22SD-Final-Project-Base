4488a31520decc09edc664a902dd1bb510166f05
Reagan615 <419247451@qq.com>
2023-05-13 18:27:26 -0500

    modify-TicketsController-index

--- BLL/TicketBL.cs
+++ BLL/TicketBL.cs
@@ -1,6 +1,19 @@
-﻿namespace SD_340_W22SD_Final_Project_Group6.BLL
+﻿using SD_340_W22SD_Final_Project_Group6.Data;
+using SD_340_W22SD_Final_Project_Group6.Models;
+using X.PagedList;
+
+namespace SD_340_W22SD_Final_Project_Group6.BLL
 {
     public class TicketBL
     {
+        private readonly IRepository<Ticket> _ticketRepository;
+        public TicketBL(IRepository<Ticket> ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+        public async Task<ICollection<Ticket>> GetAllTickets()
+        {
+            return await _ticketRepository.GetAll().ToListAsync();
+        }
     }
 }
